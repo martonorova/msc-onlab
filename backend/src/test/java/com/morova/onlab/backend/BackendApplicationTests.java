96b9552ee0101d63b4bcba7f0fd7c02bd146d64e
Marton Orova <marton.orova@gmail.com>
2020-10-18 11:29:04 +0200

    Init backend

---
+++ backend/src/test/java/com/morova/onlab/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morova.onlab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
