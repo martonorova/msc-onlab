9543600a940cb707cd198b604fde1f86675b0ab5
Marton Orova <marton.orova@gmail.com>
2020-10-19 04:00:36 +0200

    Start frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>FRONTEND</h1>
-    </div>
+    <Layout className="layout">
+    <Header>
+      {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu> */}
+    </Header>
+    <Content style={{ padding: '20px 50px'}}>
+      {/* <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb> */}
+      <div className="site-layout-content">Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
   );
 }
 
---
+++ frontend/src/context/types.js
@@ -0,0 +1,4 @@
+export const GET_JOBS = 'GET_JOBS';
+export const GET_JOB = 'GET_JOB';
+export const POST_JOB = 'POST_JOB';
+export const SET_LOADING = 'SET_LOADING';
