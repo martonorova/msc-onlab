6c2d9e7af90d0893e99838bec758c9331e38505f
Marton Orova <marton.orova@gmail.com>
2020-10-24 22:01:20 +0200

    Add reverse proxy to the stack

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,1 +1,2 @@
-export const API_URL = 'http://localhost:8000/api/v1';
+// export const API_URL = `http://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}${process.env.REACT_APP_BACKEND_API_PATH}`
+export const API_URL = '/api/v1';
