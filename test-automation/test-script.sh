4c250a44de0f70e084b6c9decb687a1ab29ec7b3
Marton Orova <morova@rapidminer.com>
2020-11-30 02:50:20 +0100

    Run first successful automated test with script

--- test-automation/test-script.sh
+++ test-automation/test-script.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# variables used in the queries
-RANGE=30m
-INTERVAL=15s
-
-### Prometheus query variables ###
-BACKEND_AVAILABILITY_QUERY="avg_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${INTERVAL}])"
-
-BACKEND_UNAVAILABILITY_QUERY="(1 - sum_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${INTERVAL}])) / count_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${INTERVAL}])"
-
-BACKEND_MUT_QUERY="15 * sum_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) / (floor((changes(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) + 1 + probe_success{instance=~'.*kubedepend-backend.*'} offset ${RANGE}) / 2))"
-
-BACKEND_MDT_QUERY="15 * (count_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) - sum_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}])) / (floor((changes(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) + 2 - probe_success{instance=~'.*kubedepend-backend.*'} offset ${RANGE}) / 2))"
-
-BACKEND_MTBF_QUERY="${BACKEND_MUT_QUERY} + ${BACKEND_MDT_QUERY}"
-
+### Metrics query (which are present on Grafana)
 # shellcheck disable=SC2089
 WORKER_BUSY_THREADS_QUERY="sum(worker_busy_threads{job='kubernetes-service-endpoints'})"
 
@@ -63,52 +49,71 @@ while ! [[ $WORKER_BUSY_THREADS_COUNT =~ "0" ]] || ! [[ $QUEUE_SIZE =~ "0" ]] ||
     QUEUE_SIZE=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${QUEUE_SIZE_QUERY}" )" | jq .data.result[0].value[1])
     WORKER_PODS_COUNT=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${WORKER_PODS_COUNT_QUERY}" )" | jq .data.result[0].value[1])
     NEEDED_WORKER_RATIO=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${NEEDED_WORKER_RATIO_QUERY}" )" | jq .data.result[0].value[1])
-    
+
 done
 
 echo "State OK, start test..."
 
-# if [ curl "http://localhost:9090/api/v1/query?query=$( urlencode 'avg_over_time(probe_success{instance=~".*kubedepend-backend.*"}[15s])' )" | jq .data.result[0].value[1] ]; then
+# apply chaos files
+echo "Creating chaos objects..."
 
-# fi
+kubectl apply -f "$(pwd)/chaos-files"
 
-# until $(curl --output /dev/null --silent --head --fail "${SSO_INTERNAL_URL}"/auth/realms/master); do
-#    echo "Waiting system to be in a stable state..."
-#    sleep 5
-# done
+echo "Chaos objects created."
 
-# # apply chaos files
-# echo "Creating chaos objects..."
+echo "Testing in progress..."
+SECONDS=0
 
-# kubectl apply -f "$(pwd)/chaos-files"
-
-# echo "Chaos objects created."
-
-# echo "Testing in progress..."
-# SECONDS=0
-
-# # artillery run ../artillery/submit_jobs.yaml
-
-# echo "Load sent, waiting for jobs to finish..."
+artillery run ../artillery/submit_jobs.yaml
 
 # sleep 2
 
-# echo "Jobs finished in $SECONDS seconds."
+echo "Load sent, waiting for jobs to finish..."
 
-# # delete chaos files
-# echo "Deleting chaos objects..."
+sleep 20
 
-# kubectl delete -f "$(pwd)/chaos-files"
+ 
+QUEUE_SIZE=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${QUEUE_SIZE_QUERY}" )" | jq .data.result[0].value[1])
+WORKER_BUSY_THREADS_COUNT=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${WORKER_BUSY_THREADS_QUERY}" )" | jq .data.result[0].value[1])
 
-# echo "Chaos objects deleted."
+# wait for busy threads == 0 and queue size == 0
+while ! { [[ $WORKER_BUSY_THREADS_COUNT =~ "0" ]] && [[ $QUEUE_SIZE =~ "0" ]]; }; do
+    echo "Workers are still busy or queue is not empty..."
+    echo "Elapsed time is: ${SECONDS} seconds"
+    sleep 5
 
-# echo "Elapsed time is"
+    QUEUE_SIZE=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${QUEUE_SIZE_QUERY}" )" | jq .data.result[0].value[1])
+    WORKER_BUSY_THREADS_COUNT=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${WORKER_BUSY_THREADS_QUERY}" )" | jq .data.result[0].value[1])
 
-# echo "Get results..."
-# echo "Range = __, Interval = __"
+done
 
-# echo "Backend availability"
-# # prometheus query
-# # curl 'http://localhost:9090/api/v1/query?query=avg_over_time%28probe_success%7Binstance%3D~%22.%2Akubedepend-backend.%2A%22%7D%5B15s%5D%29' | jq .data.result[0].value[1]
+echo "Jobs finished in $SECONDS seconds."
 
-# echo "Backend unavailability -"
+# delete chaos files
+echo "Deleting chaos objects..."
+
+kubectl delete -f "$(pwd)/chaos-files"
+
+# variables used in the queries
+RANGE=${SECONDS}s
+echo "Range is ${RANGE} seconds"
+INTERVAL=15s
+
+### Prometheus dependability metric query variables ###
+BACKEND_AVAILABILITY_QUERY="avg_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}])"
+
+BACKEND_UNAVAILABILITY_QUERY="(1 - sum_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${INTERVAL}])) / count_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${INTERVAL}])"
+
+BACKEND_MUT_QUERY="15 * sum_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) / (floor((changes(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) + 1 + probe_success{instance=~'.*kubedepend-backend.*'} offset ${RANGE}) / 2))"
+
+BACKEND_MDT_QUERY="15 * (count_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) - sum_over_time(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}])) / (floor((changes(probe_success{instance=~'.*kubedepend-backend.*'}[${RANGE}]) + 2 - probe_success{instance=~'.*kubedepend-backend.*'} offset ${RANGE}) / 2))"
+
+BACKEND_MTBF_QUERY="${BACKEND_MUT_QUERY} + ${BACKEND_MDT_QUERY}"
+
+BACKEND_AVAILABILITY=$(curl --silent "${PROMETHEUS_QUERY_URL}$( urlencode "${BACKEND_AVAILABILITY_QUERY}" )" | jq .data.result[0].value[1])
+echo "Backend availability: ${BACKEND_AVAILABILITY}"
+
+# prometheus query
+# curl 'http://localhost:9090/api/v1/query?query=avg_over_time%28probe_success%7Binstance%3D~%22.%2Akubedepend-backend.%2A%22%7D%5B15s%5D%29' | jq .data.result[0].value[1]
+
+echo "Backend unavailability -"
