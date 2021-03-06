e0156ae1719179b60496166300d5764c6ee418c2
Marton Orova <morova@rapidminer.com>
2021-03-06 18:05:32 +0100

    Configure logging

--- kubedepend/kubedepend/kubedepend.py
+++ kubedepend/kubedepend/kubedepend.py
@@ -2,12 +2,34 @@ import requests
 
 import logging
 import urllib.parse
+import time
+import os
+import sys
 
 import constants as c
 
+logging.basicConfig(
+    level=logging.INFO
+)
+
 
 def main():
-    print(is_stable_state())
+
+    args = [arg for arg in sys.argv[1:]]
+    for i, arg in enumerate(args):
+        print(i, arg)
+
+    wait_for_stable_state()
+
+    logging.info('Creating chaos objects...')
+
+
+    os.system('pwd')
+
+
+
+
+
 
 # Queries Prometheus with the given Prometheus format query
 def query_prometheus(query):
@@ -30,18 +52,24 @@ def is_stable_state():
     needed_worker_ratio = query_prometheus(c.NEEDED_WORKER_RATIO_QUERY)
 
     if worker_busy_threads_count != 0:
-        logging.warning(f'There are still busy worker threads ({worker_busy_threads_count})')
+        logging.info(f'There are still busy worker threads ({worker_busy_threads_count})')
         return False
     
     if queue_size != 0:
-        logging.warning(f'Queue is not empty ({queue_size} jobs waiting)')
+        logging.info(f'Queue is not empty ({queue_size} jobs waiting)')
         return False
 
     if worker_pods_count != 1:
-        logging.warning(f'There are multiple worker pods ({worker_pods_count})')
+        logging.info(f'There are multiple worker pods ({worker_pods_count})')
         return False
     
     return True
+
+def wait_for_stable_state():
+    while not is_stable_state():
+        logging.info('State NOT OK, waiting...')
+        time.sleep(5)
+    logging.info('State OK')
 
 if __name__ == "__main__":
     main()
