9203c5f307d8ffe6378bd9b3a01d2f78adca576e
Marton Orova <morova@rapidminer.com>
2021-10-24 20:01:00 +0200

    Add ConcurrentKafkaListenerContainerFactory Bean

--- worker/src/main/java/com/morova/onlab/worker/messaging/kafka/KafkaConsumerConfig.java
+++ worker/src/main/java/com/morova/onlab/worker/messaging/kafka/KafkaConsumerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -37,5 +38,13 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         return new DefaultKafkaConsumerFactory<>(jobConsumerConfigs());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
