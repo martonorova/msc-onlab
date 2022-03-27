99be48353f8cf2d4c6790c9a94767e6be6570a9e
Marton Orova <morova@rapidminer.com>
2022-03-27 15:44:29 +0200

    Add scripts folder

---
+++ scripts/portforward-keep-alive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# keep kubectl port-forward connection alive
+
+set -e
+
+if [[ -n $1  ]]
+then
+    port=$1
+
+    while true;
+    do
+        curl -Ss "localhost:$port" > /dev/null ; sleep 10 ;
+    done
+else
+    echo "Error: some parameters are empty"
+    exit 1
+fi
