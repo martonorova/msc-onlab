b0e90a720ced0e410b2ebc63488e39e44f5b7c51
Marton Orova <morova@rapidminer.com>
2022-03-28 10:40:34 +0200

    Restart port-forward on error

--- scripts/portforward-keep-alive.sh
+++ scripts/portforward-keep-alive.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-# keep kubectl port-forward connection alive
-
-if [[ -n $1  ]]
+if [[ -n $1 && -n $2 && -n $3 && -n $4 && -n $5 ]]
 then
-    port=$1
+    namespace=$1
+    servicename=$2
+    hostport=$3
+    targetport=$4
+    path=$5
 
+    # initial port forward
+    echo "Start initial port-forwarding"
+    kubectl -n "$namespace" port-forward service/"$servicename" "$hostport":"$targetport" &
+    pw_pid=$!
+    echo "Port-forwarding started"
+
+    sleep 5
+
+    # keep kubectl port-forward connection alive
     while true;
     do
-        curl -Ss "localhost:$port" > /dev/null ; sleep 10 ;
+        curl -Ss "localhost:$hostport$path" > /dev/null;
+
+        # if curl failed, restart portforwarding
+        if [ ! $? -eq 0 ]
+        then
+            echo "Could not connect to $servicename on port $targetport at path $path"
+            kill -9 $pw_pid
+            echo "Start new port-forwarding"
+            kubectl -n "$namespace" port-forward service/"$servicename" "$hostport":"$targetport" &
+            pw_pid=$!
+            echo "Port-forwarding started"
+        fi
+        sleep 10;
     done
 else
     echo "Error: some parameters are empty"
